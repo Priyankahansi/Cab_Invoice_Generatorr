e3dc18c85c4fb16081df3a3cd15eb569b41cd6d8
Priyanka <priyankahansi95@gmail.com>
2022-06-14 20:36:02 +0530

    added:Calculate Fare

--- src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
@@ -1,4 +1,19 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
 import static org.junit.Assert.*;
+
 public class InvoiceGeneratorTest {
-  
+
+    @org.junit.Test
+    @Test
+    public void givenDistanceAndTimeShouldReturnFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+    }
 }
