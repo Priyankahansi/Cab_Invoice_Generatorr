f7f14023a0bd2c68e4861f036e1dda8321aad866
Priyanka <priyankahansi95@gmail.com>
2022-06-14 20:42:00 +0530

    Added:Minimum_Fare

--- src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
@@ -16,4 +16,12 @@ public class InvoiceGeneratorTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
 }
